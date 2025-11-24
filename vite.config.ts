6ffc736d085c36456d43f7665aae71ccfc4f3416
miikamenk <miikarito@gmail.com>
2025-11-24 23:41:25 +0200

    forgot why this broke, testing

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
